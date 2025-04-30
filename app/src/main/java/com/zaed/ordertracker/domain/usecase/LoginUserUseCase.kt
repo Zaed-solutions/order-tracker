80bdaafea0da034105c8a1012a84889116ff3106
edrees <muhammed.edrees101@gmail.com>
2025-05-01 00:35:30 +0300

    feat: Implement login functionality in LoginUserUseCase

--- app/src/main/java/com/zaed/ordertracker/domain/usecase/LoginUserUseCase.kt
+++ app/src/main/java/com/zaed/ordertracker/domain/usecase/LoginUserUseCase.kt
@@ -1,11 +1,24 @@
 package com.zaed.ordertracker.domain.usecase
 
 import com.zaed.ordertracker.domain.repository.AuthenticationRepository
+import com.zaed.ordertracker.domain.utils.InvalidPasswordException
+import com.zaed.ordertracker.domain.utils.InvalidUsernameException
+import com.zaed.ordertracker.domain.utils.hashWithMD5
 
 class LoginUserUseCase(
     private val repo: AuthenticationRepository,
 ) {
-    suspend operator fun invoke() {
-        TODO()
+    suspend operator fun invoke(
+        username: String,
+        password: String,
+    ): Result<Unit> {
+        if (!isValidPassword(password)) return Result.failure(InvalidPasswordException())
+        if (!isValidUsername(username)) return Result.failure(InvalidUsernameException())
+        val hashedPassword = hashWithMD5(password)
+        return repo.login(username, hashedPassword)
     }
+
+    private fun isValidUsername(username: String): Boolean = username.isNotBlank() && username.length >= 3
+
+    private fun isValidPassword(password: String): Boolean = password.length >= 8
 }
